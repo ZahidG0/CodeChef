4a52965a03a981378a6bc244560177d1ee3e480d
Md. Zahid Hasan <zhjoy15@gmail.com>
2026-06-24 09:57:29 +0600

    Day -> 36

---
+++ Basic-Problem(500-800)/Too_many_items.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    cout << ((n + 9) / 10) << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
